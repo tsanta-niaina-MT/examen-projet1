23452a49627db16f5d84987c37e8b55cf9c7f43e
Tsanta-niaina-MT <hei.tsantaniaina@gmail.com>
2024-06-07 16:07:08 +0300

    feat: test of the Affirmation compound with of conjunction 'Et'

--- src/test/java/affirmationTest/TestAffirmationCompose.java
+++ src/test/java/affirmationTest/TestAffirmationCompose.java
@@ -1,2 +1,17 @@
-package affirmationTest;public class TestAffirmationCompose {
+package affirmationTest;
+
+import com.hei.school.Affirmation;
+import com.hei.school.AffirmationCompose;
+import com.hei.school.AffirmationSimple;
+import com.hei.school.Conjonction;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TestAffirmationCompose {
+    @Test
+    public void testAffirmationComposePeutEtreVraie() {
+        Affirmation gauche = new AffirmationSimple("Gauche", "true");
+        Affirmation droite = new AffirmationSimple("Droite", "false");
+        AffirmationCompose affirmation = new AffirmationCompose("Test", gauche, droite, Conjonction.Et);
+        assertEquals("faux", affirmation.peutEtreVraie());
+    }
 }
